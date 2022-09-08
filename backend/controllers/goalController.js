3a12c48cf0907c7a9285f3511fcaddc0138f3749
Yahya Abdul Majeed <yahya.zf2@gmail.com>
2022-09-08 16:27:22 +0700

    frontend setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 //Get goals
 //GET /api/goals
 //private
 const getGoals = asyncHandler(
     async (req, res)=>{
-        const goals = await Goal.find()
+        const goals = await Goal.find({user: req.user._id})
 
         res.status(200).json(goals)
     }
@@ -21,7 +22,8 @@ const setGoal = asyncHandler(
             throw new Error('Please add a new text field') 
         }
         const goal = await Goal.create({
-            text: req.body.text
+            text: req.body.text,
+            user: req.user._id,
         })
         res.status(200).json(goal)
     }
@@ -37,6 +39,19 @@ const updateGoal = asyncHandler(
             res.status(400)
             throw new Error('Goal not found')
         }
+
+        const user = await User.findById(req.user._id)
+        if(!user){
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        //make sure login user matches goal user
+        if(goal.user.toString() !== user._id){
+            res.status(401)
+            throw new Error('User not authorized')
+        }
+
         const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
         res.status(200).json(updatedGoal)
     }
@@ -52,6 +67,19 @@ const deleteGoal = asyncHandler(
             res.status(400)
             throw new Error('Goal not found')
         }
+
+        const user = await User.findById(req.user._id)
+        if(!user){
+            res.status(401)
+            throw new Error('User not found')
+        }
+
+        //make sure login user matches goal user
+        if(goal.user.toString() !== user._id){
+            res.status(401)
+            throw new Error('User not authorized')
+        }
+
         await goal.remove()
         res.status(200).json({id: req.params.id})
     }
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes')) // app.use() is for running middleware, which are functions run between the request and response
+app.use('/api/users', require('./routes/userRoutes'))
 app.use(errorHandler)
 
 app.listen(port, ()=>{
